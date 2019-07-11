7f5f83987c0b8cebe29410a92aa2a924a658162f
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-07-11 15:28:46 +0200

    can create new games

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react'
 import { onEvent } from '../actions/rooms'
 import { connect } from 'react-redux'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import * as request from 'superagent'
 import './rooms.css'
 
 class RoomsContainer extends Component {
+    state = {
+        value: ''
+    }
 
+    onSubmit = (event) => {
+        event.preventDefault()
+        console.log('hoi!!')
+        request
+            .post('http://localhost:5000/rooms')
+            .send({ name: event.target.value })
+            .end(err => console.log(err))
+        this.setState({ value: '' })
+    }
+
+    onChange = (event) => {
+        const value = event.target.value
+
+        this.setState({
+            value: value
+        })
+    }
 
     render() {
+        console.log('this.props test:', this.props.rooms)
         const rooms = this.props.rooms
-            .map((room, index) => <Link to={`/room/${index + 1}/columns`}> <div
+            .map((room, index) => <Link to={`/rooms/${index + 1}/columns`}> <div
                 key={index}
                 className="roomContainer"
-                style={{backgroundImage: `url("https://source.unsplash.com/collection/181581/480x160")`}}
+                style={{ backgroundImage: `url("https://source.unsplash.com/collection/181581/480x160")` }}
             >{room.name} ({room.id})</div> </Link>)
 
         return (
             <div>
                 Rooms
+                <br></br>
+                <form onSubmit={this.onSubmit}>
+                    <label>Create Game: </label>
+                    <input onChange={this.onChange} value={this.state.value} name="game"></input>
+                    <button>Add</button>
+                </form>
                 {rooms}
             </div>
         )
